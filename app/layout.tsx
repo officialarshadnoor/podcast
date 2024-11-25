077322b0cd8ca633e60a7692a82af49a8ed6bfff
officialarshadnoor <blogginghindi@gmail.com>
2024-11-25 11:49:06 +0530

    all

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import ConvexClerkProvider from "./providers/ConvexClerkProvider"
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Podcaster",
+  description: "Generate your podcast using AI",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ConvexClerkProvider>{children}</ConvexClerkProvider></body>
+    </html>
+  );
+}
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig =  {
+    providers: [
+        {
+            domain: "https://wired-sturgeon-41.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig;
